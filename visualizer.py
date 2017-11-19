b452d4db089c98bc76c93e2783c430e3fca775c8
Josh Zastrow <j.a.zastrow.jr@gmail.com>
2017-11-19 20:17:15 +0100

    removed commented code

--- visualizer.py
+++ visualizer.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Nov 18 16:34:35 2017
-ode used to generate output visualization
+Code used to generate output visualization
 @author: joshua
 """
 
@@ -22,9 +22,9 @@ print('Output shapes:')
 for model in ['InceptionV3', 'ResNet50', 'Xception', 'VGG16']:
     output = FeatureGenerator(model, input_shape=img[0].shape).predict(img)
     output = output[0]
-    
+
     print('\t{}: {}'.format(model, output.shape))
-    
+
     # Visualize results
     if model == 'VGG16':
         height, width = 16, 32
@@ -32,51 +32,16 @@ for model in ['InceptionV3', 'ResNet50', 'Xception', 'VGG16']:
         height, width = 32, 64
 
     row, col, _ = output.shape
-        
     output = output.reshape(row, col, height, width)
-    
+
     fig = plt.figure(figsize=(6, 3), dpi=100)
     fig.suptitle(model)
-    
     gs = gridspec.GridSpec(row, col, wspace=0.1, hspace=0.1)
-    
-    count = 1   
+
     for i in range(row):
         for j in range(col):
             axs = plt.subplot(gs[i, j])
-            # axs = fig.add_subplot(row, col, count)
-            
             axs.imshow(output[i, j, :, :], cmap='gray')
             axs.axis('off')
-            count += 1
+
     fig.savefig('{}.png'.format(model))
-        
-#        
-#        
-#from matplotlib import pyplot as plt
-#from matplotlib import gridspec
-#
-#for model, img in res.items():
-#    row, col, _ = img[0].shape
-#        
-#    if model == 'VGG16':
-#        height, width = 16, 32
-#    else:
-#        height, width = 32, 64
-#    
-#    img = img[0].reshape(row, col, height, width)
-#    
-#    fig = plt.figure(figsize=(6, 3), dpi=150)
-#    fig.suptitle(model)
-#    gs = gridspec.GridSpec(row, col, wspace=0.1, hspace=0.1)
-#    
-#    count = 1   
-#    for i in range(row):
-#        for j in range(col):
-#            axs = plt.subplot(gs[i, j])
-#            # axs = fig.add_subplot(row, col, count)
-#            
-#            axs.imshow(img[i, j, :, :], cmap='gray')
-#            axs.axis('off')
-#            count += 1
-#    fig.savefig('{}.png'.format(model))
