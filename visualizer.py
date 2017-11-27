d7d8160f74446aee181594f713c3c7881b0a48ae
Josh Zastrow <j.a.zastrow.jr@gmail.com>
2017-11-27 13:44:43 +0100

    update frames to generator function

--- visualizer.py
+++ visualizer.py
@@ -23,24 +23,17 @@ def visualize_layers():
                 include_top=False, 
                 weights='imagenet', 
                 input_shape=img[0].shape) 
+    global VGG16
     
-    layers = [layer.output for layer in VGG16.layers[:8]]
+    layers = [layer.output for layer in VGG16.layers]
     
     # Create a multiple output model
     # Each layer is an output in this model.
     activations = models.Model(input=VGG16.input,
                                output=layers).predict(img)
     
-    # Create image list
-    layer_outputs = []
-    for layer in activations:
-        for filter in range(layer.shape[3]):
-            layer_outputs.append(layer[0, :, :, filter])
-            
-    layer_outputs.reverse()
-    
     # Animate the transformations
-    fig = plt.figure(facecolor='white')
+    fig = plt.figure(facecolor='grey')
     gs1 = gridspec.GridSpec(1, 4)
     gs2 = gridspec.GridSpec(1, 4)
     gs1.update(left=0.05, right=0.95, wspace=0.05, top=0.8, bottom=0.4)
@@ -64,20 +57,19 @@ def visualize_layers():
         act_ax[i].set_yticks([])  
         
         # put an image and label in each subplot
-        images += [img_ax[i].imshow(layer_outputs[0], animated=True)]
-        labels += [img_ax[i].text(0.02, 0.90, '', 
-                   transform=img_ax[i].transAxes, 
-                   color='white')]
+        images += [img_ax[i].imshow(activations[0][0], animated=True)]
+        labels += [img_ax[i].text(.50, 0.90, '', fontsize=14,
+                                  horizontalalignment='center',
+                                  transform=img_ax[i].transAxes, 
+                                  color='white')]
   
         # add a filter activation images
         mask = np.zeros(shape=(1, activations[0].shape[3]))
         nodes += [act_ax[i].imshow(mask)]
     
-    act_num = 0
-    count = 0
     def init():
         for img in images:
-            img.set_data(layer_outputs[0])
+            img.set_data(activations[0][0])
         
         for txt in labels:
             txt.set_text('')
@@ -86,35 +78,42 @@ def visualize_layers():
             idx.set_data(mask)
         
         return images + labels + nodes
+
     
-    def plotter(x=0):
-        global act_num, count
-        while True:
-            while act_num < len(activations):
-                num_filters = activations[act_num].shape[3]
-                while count < num_filters:
-                    for img in images:
-                        img.set_data(activations[act_num][0, :, :, count])
-                    
-                    for txt in labels:
-                        txt.set_text('activation {}'.format(act_num))
-                    
-                    for i in range(4):
-                        mask = np.zeros(shape=(1, num_filters))
-                        mask[0, count] = 1
-                        nodes[i] = act_ax[i].imshow(mask)
-                        
-                    res = images + labels + nodes
-                    
-                    count += 1  # next filter
-                    return res
+    def layer_generator():
+        VGG16_layers = zip(VGG16.layers, activations)
+        
+        for layer, output in VGG16_layers:
+            name = layer.name
+            output_filters = output.shape[3]
+
+            for i in range(output_filters):
+                filter_image = output[0, :, : , i]
+                filter_mask = np.zeros(shape=(1, output_filters))
                 
-                count = 0
-                act_num +=1
-            act_num = 0
-    
+                if output_filters % 64 == 0:
+                    filter_mask = filter_mask.reshape((-1, 64))
+                
+                filter_mask.itemset(i, 1)
+                yield [name, filter_image, filter_mask]
+                          
+                
+    def plotter(args):
+        name, output, mask = args
+        
+        for img in images:
+            img.set_data(output)
+        for txt in labels:
+            txt.set_text(name)
+        for i in range(4):
+            nodes[i] = act_ax[i].imshow(mask)
+            
+        res = images + labels + nodes
+        
+        return res
+        
     anim = animation.FuncAnimation(fig, plotter,  init_func=init,
-                                   frames=400,
+                                   frames=layer_generator,
                                    interval=5,
                                    blit=True,
                                    repeat=False)
